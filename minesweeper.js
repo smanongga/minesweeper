fbd57c53f6cb7490db0620c5cccf2752b227a2ac
S. Manongga <smanongga@gmail.com>
2017-04-19 11:47:50 +1200

    fix bug to chooseMap function

--- minesweeper.js
+++ minesweeper.js
@@ -1,32 +1,74 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-  cells: [
-    {row:0,col:0, isMine:true, hidden:true},
-    {row:0,col:1, isMine:false, hidden:true},
-    {row:0,col:2, isMine:false, hidden:true},
-    {row:1,col:0, isMine:false, hidden:true},
-    {row:1,col:1, isMine:true, hidden:true},
-    {row:1,col:2, isMine:false, hidden:true},
-    {row:2,col:0, isMine:false, hidden:true},
-    {row:2,col:1, isMine:false, hidden:true},
-    {row:2,col:2, isMine:false, hidden:true}]
+var board = new Object()
+  board.cells = [
 
-};
-
+  ];
 
 function startGame () {
-  for (var i = 0; i < board.cells.length; i++) {
-    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
 
+  for (var i = 0; i < board.cells.length; i++){
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
+
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin);
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
-  document.addEventListener('click', checkForWin)
-  document.addEventListener('contextmenu', checkForWin)
+
+  // var reset = document.getElementById('reset');
+  //  reset.addEventListener('click', function(evt) { location.reload() })
+}
+
+var chooseMap = function(a) {
+  if(a === undefined) {
+    return 1;
+  }
+  else {
+    return 3;
+  }
+}
+
+
+     switch(chooseMap()) {
+       case 1:
+       createBoard (3,3);
+         break;
+       case 2:
+       createBoard (4,4);
+         break;
+       case 3:
+       createBoard (5,5);
+         break;
+       case 4:
+       createBoard (6,6);
+         break;
+       default:
+       createBoard (3,3);
 
 }
+
+
+// Generate Minsweeper Board
+
+function createBoard (row,col) {
+  for (var a = 0; a < row; a++) {
+    for (var b = 0; b < col; b++) {
+      board.cells.push({
+        row: a,
+        col: b,
+        isMine: Boolean(Math.floor(Math.random()*1.3)),
+        isMarked: false,
+        hidden: true
+      })
+      console.log(board.cells)
+    }
+  }
+}
+
+
 
 // Define this function to look for a win condition:
 //
@@ -45,7 +87,9 @@ for (var i = 0; i < board.cells.length; i++) {
   // detected that they've won, that is!)
   // lib.displayMessage('You win!')
   lib.displayMessage('You win!')
+
 }
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -56,12 +100,12 @@ for (var i = 0; i < board.cells.length; i++) {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
-  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
-  var count = 0
-  for (var i = 0; i < surrounding.length; i++){
-        if (surrounding[i].isMine === true){
-          count +=1
-        }
-      }
-      return count
+  count = 0;
+var surroundingCells = getSurroundingCells(cell.row, cell.col);
+for(var i = 0; i < surroundingCells.length; i++){
+  if (surroundingCells[i].isMine === true){
+    count++;
   }
+}
+return count;
+}
